9466cc7892798332485b53721fb1f8e211f5ddf7
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-10-11 10:08:28 +0200

    Fix typo.

--- scripts/count-matrix.py
+++ scripts/count-matrix.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-matrix = pd.concat([pd.read_table(f, index_col=0)[1] for f in snakmake.input], 
+matrix = pd.concat([pd.read_table(f, index_col=0)[1] for f in snakemake.input], 
                    axis=1, names=snakemake.params.samples)
 matrix.to_csv(snakemake.output[0], sep="\t")
