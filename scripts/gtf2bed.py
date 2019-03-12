190e628ff97ff581e67e258eaf5410b8635c3b19
Johannes Köster <johannes.koester@tu-dortmund.de>
2019-03-12 09:39:02 +0100

    Use context for file obj.

--- scripts/gtf2bed.py
+++ scripts/gtf2bed.py
@@ -1,4 +1,5 @@
 import gffutils
+
 db = gffutils.create_db(snakemake.input[0],
                         dbfn=snakemake.output.db,
                         force=True,
@@ -8,9 +9,8 @@ db = gffutils.create_db(snakemake.input[0],
                         disable_infer_genes=True,
                         disable_infer_transcripts=True)
 
-outfileobj = open(snakemake.output.bed, 'w')
-for tx in db.features_of_type('transcript', order_by='start'):
-            bed = [s.strip() for s in db.bed12(tx).split('\t')]
-            bed[3] = tx.id
-            outfileobj.write('{}\n'.format('\t'.join(bed)))
-outfileobj.close()
+with open(snakemake.output.bed, 'w') as outfileobj:
+    for tx in db.features_of_type('transcript', order_by='start'):
+        bed = [s.strip() for s in db.bed12(tx).split('\t')]
+        bed[3] = tx.id
+        outfileobj.write('{}\n'.format('\t'.join(bed)))
