82b1317bb20134bcd17316f5a5ebbadd9facb66e
Jose Maturana <jlmaturanag@ug.uchile.cl>
2019-06-07 20:43:39 -0700

    Update count-matrix.py

--- scripts/count-matrix.py
+++ scripts/count-matrix.py
@@ -1,23 +1,20 @@
 import pandas as pd
 
-def strandedness(strand_vals):
-    strandedness_list = []
-    for strand_val in snakemake.params.strand:
-        if pd.isnull(strand_val) or strand_val == "none":
-            strandedness_list.append(1) #non stranded protocol
-        elif strand_val == "yes":
-            strandedness_list.append(2) #3rd column
-        elif strand_val == "reverse":
-            strandedness_list.append(3) #4th column, usually for Illumina truseq
-        else:
-            raise ValueError(("'strandedness' column should be empty or have the " 
-                "value 'none', 'yes' or 'reverse', instead has the value {}").format(repr(strand_val)))
-    
-    return strandedness_list
+def get_column(strandedness):
+    if pd.isnull(strandedness) or strandedness == "none":
+        return 1 #non stranded protocol
+    elif strandedness == "yes":
+        return 2 #3rd column
+    elif strandedness == "reverse":
+        return 3 #4th column, usually for Illumina truseq
+    else:
+        raise ValueError(("'strandedness' column should be empty or have the " 
+                          "value 'none', 'yes' or 'reverse', instead has the " 
+                          "value {}").format(repr(strandedness)))
 
-stranded_list = strandedness(snakemake.params.strand)
-counts = [pd.read_table(f, index_col=0, usecols=[0, coln], header=None, skiprows=4) \
-for f, coln in zip(snakemake.input, stranded_list)]
+counts = [pd.read_table(f, index_col=0, usecols=[0, get_column(strandedness)], 
+          header=None, skiprows=4) 
+          for f, strandedness in zip(snakemake.input, snakemake.params.strand)]
 
 for t, sample in zip(counts, snakemake.params.samples):
     t.columns = [sample]
