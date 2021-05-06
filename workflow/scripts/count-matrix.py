e76fcfbb5d0532d060d61526e2332f2e1c75c7f2
Johannes Köster <johannes.koester@tu-dortmund.de>
2021-05-06 23:34:39 +0200

    formatting

--- workflow/scripts/count-matrix.py
+++ workflow/scripts/count-matrix.py
@@ -1,24 +1,34 @@
 import sys
+
 # logging
 sys.stderr = open(snakemake.log[0], "w")
 
 import pandas as pd
 
+
 def get_column(strandedness):
     if pd.isnull(strandedness) or strandedness == "none":
-        return 1 #non stranded protocol
+        return 1  # non stranded protocol
     elif strandedness == "yes":
-        return 2 #3rd column
+        return 2  # 3rd column
     elif strandedness == "reverse":
-        return 3 #4th column, usually for Illumina truseq
+        return 3  # 4th column, usually for Illumina truseq
     else:
-        raise ValueError(("'strandedness' column should be empty or have the " 
-                          "value 'none', 'yes' or 'reverse', instead has the " 
-                          "value {}").format(repr(strandedness)))
+        raise ValueError(
+            (
+                "'strandedness' column should be empty or have the "
+                "value 'none', 'yes' or 'reverse', instead has the "
+                "value {}"
+            ).format(repr(strandedness))
+        )
 
-counts = [pd.read_table(f, index_col=0, usecols=[0, get_column(strandedness)], 
-          header=None, skiprows=4) 
-          for f, strandedness in zip(snakemake.input, snakemake.params.strand)]
+
+counts = [
+    pd.read_table(
+        f, index_col=0, usecols=[0, get_column(strandedness)], header=None, skiprows=4
+    )
+    for f, strandedness in zip(snakemake.input, snakemake.params.strand)
+]
 
 for t, sample in zip(counts, snakemake.params.samples):
     t.columns = [sample]
--- workflow/scripts/gtf2bed.py
+++ workflow/scripts/gtf2bed.py
@@ -1,16 +1,18 @@
 import gffutils
 
-db = gffutils.create_db(snakemake.input[0],
-                        dbfn=snakemake.output.db,
-                        force=True,
-                        keep_order=True,
-                        merge_strategy='merge',
-                        sort_attribute_values=True,
-                        disable_infer_genes=True,
-                        disable_infer_transcripts=True)
+db = gffutils.create_db(
+    snakemake.input[0],
+    dbfn=snakemake.output.db,
+    force=True,
+    keep_order=True,
+    merge_strategy="merge",
+    sort_attribute_values=True,
+    disable_infer_genes=True,
+    disable_infer_transcripts=True,
+)
 
-with open(snakemake.output.bed, 'w') as outfileobj:
-    for tx in db.features_of_type('transcript', order_by='start'):
-        bed = [s.strip() for s in db.bed12(tx).split('\t')]
+with open(snakemake.output.bed, "w") as outfileobj:
+    for tx in db.features_of_type("transcript", order_by="start"):
+        bed = [s.strip() for s in db.bed12(tx).split("\t")]
         bed[3] = tx.id
-        outfileobj.write('{}\n'.format('\t'.join(bed)))
+        outfileobj.write("{}\n".format("\t".join(bed)))
