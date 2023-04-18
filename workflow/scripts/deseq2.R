1e233e326989c09f4dd549932989a2743f01ba24
David Laehnemann <david.laehnemann@hhu.de>
2023-04-18 21:14:55 +0200

    apply lints to deseq2.R

--- workflow/scripts/deseq2.R
+++ workflow/scripts/deseq2.R
@@ -1,6 +1,6 @@
-log <- file(snakemake@log[[1]], open="wt")
+log <- file(snakemake@log[[1]], open = "wt")
 sink(log)
-sink(log, type="message")
+sink(log, type = "message")
 
 library("DESeq2")
 
@@ -15,20 +15,37 @@ if (snakemake@threads > 1) {
 dds <- readRDS(snakemake@input[[1]])
 
 contrast <- c("condition", snakemake@params[["contrast"]])
-res <- results(dds, contrast=contrast, parallel=parallel)
+res <- results(
+  dds,
+  contrast = contrast,
+  parallel = parallel
+)
 # shrink fold changes for lowly expressed genes
-# use ashr so we can use `contrast` as conversion to coef is not trivial
-# see https://bioconductor.org/packages/release/bioc/vignettes/DESeq2/inst/doc/DESeq2.html#extended-section-on-shrinkage-estimators
-res <- lfcShrink(dds, contrast=contrast, res=res, type="ashr")
+# use ashr so we can use `contrast` as conversion to coef is not trivial, see:
+# https://bioconductor.org/packages/release/bioc/vignettes/DESeq2/inst/doc/DESeq2.html#extended-section-on-shrinkage-estimators
+res <- lfcShrink(
+  dds,
+  contrast = contrast,
+  res = res,
+  type = "ashr"
+)
 
 # sort by p-value
-res <- res[order(res$padj),]
+res <- res[order(res$padj), ]
 # TODO explore IHW usage
 
 
 # store results
 svg(snakemake@output[["ma_plot"]])
-plotMA(res, ylim=c(-2,2))
+plotMA(res, ylim = c(-2, 2))
 dev.off()
 
-write.table(data.frame("gene"=rownames(res),res), file=snakemake@output[["table"]], row.names=FALSE, sep='\t')
+write.table(
+  data.frame(
+    "gene" = rownames(res),
+    res
+  ),
+  file = snakemake@output[["table"]],
+  row.names = FALSE,
+  sep = "\t"
+)
